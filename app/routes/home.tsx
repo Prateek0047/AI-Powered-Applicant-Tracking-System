feac846874027ac39e665bf3c2fcdeb5817b419e
Prateek undead <pr805201@gmail.com>
2025-08-04 15:00:44 +0530

    authentication done using puter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,8 +2,11 @@ import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 import Navbar from "~/Components/Navbar";
 import { resumes } from "../../constants";
+import { useEffect } from "react";
 
 import ResumeCards from "~/Components/ResumeCards";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,11 +16,18 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate("/auth?next=/");
+  }, [auth.isAuthenticated]);
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1> Track your Application and resume rating </h1>
           <h2> Review your submission and check AI powered feedback </h2>
         </div>
